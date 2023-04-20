e8368dedcdfa330c4c0becbdc48b82a85cf1fc24
Dylan Glo Ignacio <70607367+Dylan-Glo@users.noreply.github.com>
2023-04-19 17:52:58 -0700

    Added files

---
+++ IOS_Project_2_Flixster/IOS_Project_2_Flixster/PosterCell.swift
@@ -0,0 +1,13 @@
+//
+//  PosterCell.swift
+//  IOS_Project_2_Flixster
+//
+//  Created by Dylan Ignacio on 4/15/23.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
